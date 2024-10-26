8ae0a6f7ee4ab58eedfc51a4d51ffa8ede19efdc
Lisetvdheuvel <lisetvdheuvel2003@outlook.com>
2024-10-26 16:30:44 +0200

    Geluid

---
+++ Game IMST/Assets/scripts/Fontein.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fontein : MonoBehaviour
+{
+    
+AudioSource audioSource;
+
+public AudioClip fontein;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void Afspelen(){
+
+        audioSource.PlayOneShot(fontein);
+    }
+
+
+}
