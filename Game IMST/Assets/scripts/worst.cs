8e79846d0753122efb8f58d96740f0134eef2e6f
Lisetvdheuvel <lisetvdheuvel2003@outlook.com>
2024-10-28 13:24:44 +0100

    Vlees oppakbaar maken

---
+++ Game IMST/Assets/scripts/worst.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class worst : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    public void Oppakken(){
+    gameObject.SetActive(false);
+    }
+}
