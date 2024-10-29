8dfd6d5f79c45e13e531a81c4d91520d4c7693d1
FinnvdW <finnvdwallen@gmail.com>
2024-10-29 12:27:36 +0100

    deur slager

--- Game IMST/Assets/scripts/DeurSlager.cs
+++ Game IMST/Assets/scripts/DeurSlager.cs
@@ -7,21 +7,29 @@ public class DeurSlager : MonoBehaviour
     Animator animator;
     AudioSource audioSource;
     public AudioClip DeurOpen;
+    public bool DeurIsOpen;
+    public bool DeurIsDicht;
 
     public AudioClip DeurDicht;
 
     void Start(){
         animator = GetComponent<Animator>();
         audioSource = GetComponent<AudioSource>();
+        DeurIsOpen = false;
     }
 
     public void OpenDeur(){
-        animator.SetTrigger("Openen");
-        audioSource.PlayOneShot(DeurOpen);
+        if (DeurIsOpen == false){
+            animator.SetTrigger("openen");
+            audioSource.PlayOneShot(DeurOpen);
+            DeurIsOpen = true;
+        }
+        
+        if (DeurIsOpen == true){
+            animator.SetTrigger("sluiten");
+            audioSource.PlayOneShot(DeurDicht);
+            DeurIsOpen = false;
+        }
     }
 
-    public void DichtDeur(){
-        animator.SetTrigger("Sluiten");
-        audioSource.PlayOneShot(DeurDicht);
-    }
 }
