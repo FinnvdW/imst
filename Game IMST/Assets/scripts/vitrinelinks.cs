364871ebbc9b931f827c8292a9f6d8d0113fa7fd
FinnvdW <finnvdwallen@gmail.com>
2024-10-30 23:57:15 +0100

    vitrine werkt

---
+++ Game IMST/Assets/scripts/vitrinelinks.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vitrinelinks : MonoBehaviour
+{
+    Animator animator;
+    AudioSource audioSource;
+    public AudioClip DeurOpen;
+    public bool DeurIsOpen;
+    public AudioClip DeurDicht;
+
+    void Start(){
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+        DeurIsOpen = false;
+    }
+
+    public void OpenDeur(){
+        if (DeurIsOpen == false){
+            animator.SetTrigger("open");
+            audioSource.PlayOneShot(DeurOpen);
+            DeurIsOpen = true;
+            GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<BoxCollider>().enabled = false;
+            StartCoroutine(ColliderTerug());
+        }else{
+            animator.SetTrigger("dicht");
+            audioSource.PlayOneShot(DeurDicht);
+            GetComponent<MeshRenderer>().enabled = true;
+            GetComponent<BoxCollider>().enabled = true;
+            DeurIsOpen = false;
+        }
+    }
+
+    IEnumerator ColliderTerug(){
+        yield return new WaitForSeconds(2f);
+        GetComponent<MeshRenderer>().enabled = true;
+        GetComponent<BoxCollider>().enabled = true;
+    }
+}
