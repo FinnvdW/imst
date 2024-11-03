99ceec44ff95532560fdcc6e30899968c1ce6829
FinnvdW <finnvdwallen@gmail.com>
2024-11-03 15:25:29 +0100

    coole boer shit

---
+++ Game IMST/Assets/scripts/NaarStadBoer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NaarStadBoer : MonoBehaviour
+{
+    void OnTriggerEnter(){
+        SceneManager.LoadScene("stad", LoadSceneMode.Single);
+    }
+}
