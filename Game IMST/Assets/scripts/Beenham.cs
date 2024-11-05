5daf7fee9bb4e2a6036ab8ffea04519c1c0b270b
FinnvdW <finnvdwallen@gmail.com>
2024-11-05 11:28:03 +0100

    wait.. they dont love you like i love you

---
+++ Game IMST/Assets/scripts/Beenham.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beenham : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    public void Pakken()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Game IMST/Assets/scripts/Worst2.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Worst2 : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    public void Pakken()
+    {
+        gameObject.SetActive(false);
+    }
+}
