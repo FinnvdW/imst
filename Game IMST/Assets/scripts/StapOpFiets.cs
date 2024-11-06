7c1b9a348b05a66c991bb20edad6412a7a89c5ec
FinnvdW <finnvdwallen@gmail.com>
2024-11-06 12:44:12 +0100

    fietssleutel

--- Game IMST/Assets/scripts/StapOpFiets.cs
+++ Game IMST/Assets/scripts/StapOpFiets.cs
@@ -7,13 +7,17 @@ public class PakFiets : MonoBehaviour
 {
     AudioSource opStappen;
     public AudioClip StapOp;
+    public bool HeeftFietsSleutel;
+    public Speler speler;
 
     void Start(){
         opStappen = GetComponent<AudioSource>();
     }
 
     public void StapOpFiets(){
-        opStappen.PlayOneShot(StapOp);
-        SceneManager.LoadScene("Stad + fiets", LoadSceneMode.Single);
+        if(speler.HeeftFietsSleutel == true){
+            opStappen.PlayOneShot(StapOp);
+            SceneManager.LoadScene("Stad + fiets", LoadSceneMode.Single);
+        }
     }
 }
