8761db6ad9191d4ce04c6c42ab7fc3fc52177667
FinnvdW <finnvdwallen@gmail.com>
2024-11-09 22:20:52 +0100

    vitrine

--- Game IMST/Assets/scripts/pratenJimmy.cs
+++ Game IMST/Assets/scripts/pratenJimmy.cs
@@ -67,43 +67,43 @@ public class pratenJimmy : MonoBehaviour
         yield return new WaitForSeconds(3.1f);
         GordonTalking.enabled = true;
         Gordon.enabled = true;
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(3.5f);
         GordonTalking.enabled = false;
         Gordon.enabled = false;
     }
 
     IEnumerator Tekstweg2(){
-        yield return new WaitForSeconds(6.1f);
+        yield return new WaitForSeconds(6.7f);
         JimmyTalking1.enabled = true;
         Jimmy.enabled = true;
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(4.5f);
         JimmyTalking1.enabled = false;
         Jimmy.enabled = false;
     }
 
     IEnumerator GordonYapping2(){
-        yield return new WaitForSeconds(9.1f);
+        yield return new WaitForSeconds(11.3f);
         GordonTalking1.enabled = true;
         Gordon.enabled = true;
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(5f);
         GordonTalking1.enabled = false;
         Gordon.enabled = false;
     }
 
     IEnumerator Tekstweg3(){
-        yield return new WaitForSeconds(12.1f);
+        yield return new WaitForSeconds(16.4f);
         JimmyTalking2.enabled = true;
         Jimmy.enabled = true;
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(4.5f);
         JimmyTalking2.enabled = false;
         Jimmy.enabled = false;
     }
 
     IEnumerator GordonYapping3(){
-        yield return new WaitForSeconds(15.1f);
+        yield return new WaitForSeconds(21f);
         GordonTalking2.enabled = true;
         Gordon.enabled = true;
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(4.5f);
         GordonTalking2.enabled = false;
         Gordon.enabled = false;
     }
