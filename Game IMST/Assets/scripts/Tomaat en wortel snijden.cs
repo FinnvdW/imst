c4dd065b058bd70cdd045646e291c0e5c0f9f958
FinnvdW <finnvdwallen@gmail.com>
2024-11-12 16:04:47 +0100

    epische shit ouwe

--- Game IMST/Assets/scripts/Tomaat en wortel snijden.cs
+++ Game IMST/Assets/scripts/Tomaat en wortel snijden.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Tomaatenwortelsnijden : MonoBehaviour
 {
-    // Start is called before the first frame update
+    AudioSource audioSource;
+    public AudioClip snijden;
     void Start()
     {
         GetComponent<MeshRenderer>().enabled = false;
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     public void Snijden()
     {
+        audioSource.PlayOneShot(snijden);
         GetComponent<MeshRenderer>().enabled = true;
+        GetComponent<BoxCollider>().enabled = false;
     }
 }
